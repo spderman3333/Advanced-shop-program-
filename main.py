fded442dfa3734a59eca69cb5011b51f45e5b86e
spderman333 <118777573+spderman3333@users.noreply.github.com>
2022-12-09 01:57:59 +0000

    Begining of rehaul.

--- main.py
+++ main.py
@@ -2,31 +2,10 @@ from replit import db
 import os
 import time
 
-print("Welcome to Stefan's Bank")
+def logo():
+  print("<insert bank name here>")
+  print()
+  print("--------------")
+  print()
 
-def login():
-  pass
-
-def register():
-  pass
-
-def Error():
-  print("\033[31m")
-  print("Error, Command not found!")
-  print("\033[0m")
-
-while True:
-  print("Please choose one of these options:")
-  print("1) login into existing account")
-  print("2) register new account")
-  option = int(input("Choose option: "))
-  if option == 1:
-    login()
-    break
-  elif option == 2:
-    register()
-    break
-  else:
-    Error()
-    
-# db["Bank"] = Money
+logo()
